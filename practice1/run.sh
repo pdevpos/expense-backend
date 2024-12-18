96fab4f06f0e0adf52e079c60a2b05e3e20293a4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-18 15:47:49 +0530

    added source path

--- practice1/run.sh
+++ practice1/run.sh
@@ -2,8 +2,5 @@ source /test/secrets.txt
 #echo "getting secrets from vault" /test/secrets.txt
 cat /test/secrets.txt
 if [ -z "$RDB_HOST" ]; then
- echo DB_HOST is missing
-fi
-if [  ]; then
-    
+ echo RDB_HOST is missing
 fi
